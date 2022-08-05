aaca8362301eb001bbf8fa7109a9c03f896de99d
Andrei Staravoitau <andrei.starovojtov@gmail.com>
2022-08-05 23:08:16 +0300

    logging

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const banano = require("@bananocoin/bananojs");
 const database = require("./db");
 const config = {
   faucetWallet:
-    "ban_3gahaiusraz8qnotf3skqn3myo74o9f7hroqw8hhny51zkkx5ikbxsbat69c",
+    "ban_3f1o95qeeg1zignw11ew5sfaxhzogsj3hzm377xjtmab8hwz535p6f96i5uu",
   claimAmount: 0.01,
   claimCooldown: 24 * 60 * 60 * 1000,
   oneBanRaw: 100000000000000000000000000000,
@@ -25,14 +25,12 @@ app.get("/users", async function (req, res) {
   const sql = "SELECT * FROM Users";
   const getAll = await database.all(sql);
   res.json(await getAll);
-  //res.sendFile(path.join(__dirname, "/static/index.html"));
 });
 
 app.get("/balance", async function (req, res) {
   const balance = await banano.getAccountInfo(config.faucetWallet);
   
   res.json({ balance: balance.balance_decimal });
-  //res.sendFile(path.join(__dirname, "/static/index.html"));
 });
 
 app.post("/claim", async function (req, res) {
@@ -43,39 +41,47 @@ app.post("/claim", async function (req, res) {
   
   if (!event.isTrusted) {
     res.json({ error: "Somtheing vary bad happened!" });
+    console.log('Event is not trusted');
     return;
   }
+  console.log('Event is trusted');
 
   if (!address) {
     res.json({ error: "Provide valid BAN address to continue" });
+    console.log('Ban address was not provided');
     return;
   }
-  // banano.bananodeApi.setUrl("https://kaliumapi.appditto.com/api");
+  console.log('Address is present');
 
   /* 1. Validate address */
   const bAddressValid = await banano.getBananoAccountValidationInfo(address)
     .valid;
   if (!bAddressValid) {
     res.status(500).json({ error: "Invalid BAN address" });
+    console.log('Address is invalid');
     return;
   }
+  console.log('Address is valid');
 
   /* 2. Unopened */
   const accHistory = await banano.getAccountHistory(address, -1);
   const bIsUnopened = accHistory.history.length === 0;
   if (bIsUnopened) {
     res.status(418).json({ error: "Unopened account" });
+    console.log('Account is unopened');
     return;
   }
+  console.log('Account is opened');
 
   /* 3. Balance */
   const faucetWalletInfo = await banano.getAccountInfo(config.faucetWallet);
 
   if (Number(faucetWalletInfo.balance_decimal) < config.claimAmount) {
     res.status(500).json({ error: "Balance too low. Please try again later" });
+    console.log('Balance low');
     return;
   }
-
+  console.log('Balance is sufficient');
   /* 4. Check wether this address already grabbed a claim */
 
   const sql = `SELECT * FROM Users WHERE address='${address}'`;
@@ -87,15 +93,17 @@ app.post("/claim", async function (req, res) {
 
   if (!claimCooldownPassed(user.lastClaim)) {
     res.status(418).json({ error: "Come back later" });
+    console.log('User is on cooldown');
     return;
   }
-
+  console.log('User is not on cooldown');
   // 6. Check wether address is blacklisted
   if (user.banned) {
     res.status(418).json({ error: "You were banned. Bye" });
+    console.log('User banned');
     return;
   }
-
+  console.log('User is not banned');
   // 7. Check same IP-address already grabbed
 
   const usersWithSameIp = await database.all(
@@ -112,14 +120,26 @@ app.post("/claim", async function (req, res) {
 
     if (!claimCooldownPassed(theUser.lastClaim)) {
       res.status(418).json({ error: "Come back later" });
+      console.log('Same IP check failed');
       return;
     }
   }
+  console.log('IP check passed');
   // 8. All good. Send
   
+  try {
   banano.sendAmountToBananoAccount(config.seed, 0 /* index */, address, config.claimAmountRaw, 
     result =>  res.json(result),
-    err => res.status(500).json({ error: err }));
+    err => {
+      res.status(500).json({ error: err } )
+      console.log('Send banano failed uncatched');  
+    });
+  } catch (e) {
+    res.status(500).json({ error: err });
+    console.log('Send banano failed and catched');
+  }
+
+  // 8.1 Check transaction?!
 
   // 9. Update user ip, lastClaim
   const setIpSql = `UPDATE Users SET ip = '${currentIp}', lastClaim = '${new Date().toISOString()}' WHERE address ='${address}'`;
@@ -128,6 +148,7 @@ app.post("/claim", async function (req, res) {
     .then(() => {
       //updates silently
       // res.json({ message: "Successful claim"});
+      console.log('User updated after a claim');
     })
     .catch((err) => {
       console.log(err);
